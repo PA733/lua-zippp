c0851fda7927683e9c85e087578addee44f01b5e
pa733 <2291750588@qq.com>
2023-01-07 14:35:33 +0800

    feat: cmake

---
+++ lua_zippp.cpp
@@ -0,0 +1,49 @@
+﻿#define LUA_LIB
+#define LUA_BUILD_AS_DLL
+
+
+
+#include "lua.hpp"
+#pragma comment(lib, "lua.lib")
+
+#pragma once
+#define DllExport   __declspec( dllexport )//宏定义
+#ifndef zippp_H
+#define zippp_H
+
+class DllExport zippp
+{
+
+};
+
+#endif
+
+static int sum(lua_State* L) {
+	int num1 = lua_tointeger(L, 1);
+	int num2 = lua_tointeger(L, 2);
+	int result;
+	result = num1 + num2;
+	lua_pushnumber(L, result);
+	return 1;
+}
+
+static struct luaL_Reg ls_lib[] = {
+	{"sum", sum},
+	{nullptr, nullptr}
+};
+
+extern "C" LUALIB_API int luaopen_lua_zippp(lua_State * L) {
+#if LUA_VERSION_NUM >= 502
+	luaL_newlib(L, ls_lib);
+#else
+	luaL_openlib(L, "lua_zippp", ls_lib, 0);
+#endif
+
+	return 1;
+}
+
+extern "C" {
+	int sum(int x, int y) {
+		return x + y;
+	}
+}
